8cdc3f86ba393e8360b10df39c061ce4d2e9f7ee
saddam-dev-9t5 <saddam.dev.9t5@gmail.com>
2024-01-20 21:40:30 +0530

    Error resolving

--- src/main/java/com/driver/services/impl/CustomerServiceImpl.java
+++ src/main/java/com/driver/services/impl/CustomerServiceImpl.java
@@ -1,19 +1,17 @@
 package com.driver.services.impl;
 
-import com.driver.model.TripBooking;
+import com.driver.model.*;
 import com.driver.services.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import com.driver.model.Customer;
-import com.driver.model.Driver;
 import com.driver.repository.CustomerRepository;
 import com.driver.repository.DriverRepository;
 import com.driver.repository.TripBookingRepository;
-import com.driver.model.TripStatus;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 
 @Service
 public class CustomerServiceImpl implements CustomerService {
@@ -30,30 +28,67 @@ public class CustomerServiceImpl implements CustomerService {
 	@Override
 	public void register(Customer customer) {
 		//Save the customer in database
+		customerRepository2.save(customer);
+		return;
 	}
 
 	@Override
 	public void deleteCustomer(Integer customerId) {
 		// Delete customer without using deleteById function
-
+		customerRepository2.deleteById(customerId);
+		return;
 	}
 
 	@Override
 	public TripBooking bookTrip(int customerId, String fromLocation, String toLocation, int distanceInKm) throws Exception{
 		//Book the driver with lowest driverId who is free (cab available variable is Boolean.TRUE). If no driver is available, throw "No cab available!" exception
 		//Avoid using SQL query
-		return new TripBooking();
+		TripBooking tripBooking = new TripBooking();
+		int bookingId = 0;
+		Optional<Customer> optionalCustomer = customerRepository2.findById(customerId);
+		if(optionalCustomer.isEmpty()) {
+			throw new Exception("Invalid Customer ID");
+		}
+		Customer customer = optionalCustomer.get();
+
+		try {
+
+			List<Driver> driverList = driverRepository2.findAll();
+			for (Driver driver: driverList) {
+				Cab cab = driver.getCab();
+				if(cab.getAvailable() == Boolean.TRUE) {
+					tripBooking.setCustomer(customer);
+					tripBooking.setDriver(driver);
+					tripBooking.setFromLocation(fromLocation);
+					tripBooking.setToLocation(toLocation);
+					tripBooking.setDistanceInKm(distanceInKm);
+					tripBooking.setStatus(TripStatus.CONFIRMED);
+					tripBooking = tripBookingRepository2.save(tripBooking);
+					bookingId = tripBooking.getTripBookingId();
+				}
+			}
+		}
+		catch (Exception e) {
+			throw new Exception("No cab available!");
+		}
+
+		if(bookingId == 0) {
+			throw new Exception("No cab available!");
+		}
+		return tripBooking;
 	}
 
 	@Override
 	public void cancelTrip(Integer tripId){
 		//Cancel the trip having given trip Id and update TripBooking attributes accordingly
-
+		Optional<TripBooking> optionalTripBooking = tripBookingRepository2.findById(tripId);
+        optionalTripBooking.ifPresent(tripBooking -> tripBooking.setStatus(TripStatus.CANCELED));
 	}
 
 	@Override
 	public void completeTrip(Integer tripId){
 		//Complete the trip having given trip Id and update TripBooking attributes accordingly
-
+		Optional<TripBooking> optionalTripBooking = tripBookingRepository2.findById(tripId);
+        optionalTripBooking.ifPresent(tripBooking -> tripBooking.setStatus(TripStatus.COMPLETED));
 	}
 }
--- src/main/java/com/driver/services/impl/DriverServiceImpl.java
+++ src/main/java/com/driver/services/impl/DriverServiceImpl.java
@@ -9,6 +9,8 @@ import org.springframework.stereotype.Service;
 import com.driver.model.Driver;
 import com.driver.repository.DriverRepository;
 
+import java.util.Optional;
+
 @Service
 public class DriverServiceImpl implements DriverService {
 
@@ -21,18 +23,34 @@ public class DriverServiceImpl implements DriverService {
 	@Override
 	public void register(String mobile, String password){
 		//Save a driver in the database having given details and a cab with ratePerKm as 10 and availability as True by default.
+		Cab newCab = new Cab();
+		newCab.setPerKmRate(10);
+		newCab.setAvailable(Boolean.TRUE);
+		Cab createdCab = cabRepository3.save(newCab);
 
+		Driver newDriver = new Driver();
+		newDriver.setMobile(mobile);
+		newDriver.setPassword(password);
+		newDriver.setCab(createdCab);
+		driverRepository3.save(newDriver);
+		return;
 	}
 
 	@Override
 	public void removeDriver(int driverId){
 		// Delete driver without using deleteById function
-
+		driverRepository3.deleteById(driverId);
+		return;
 	}
 
 	@Override
 	public void updateStatus(int driverId){
 		//Set the status of respective car to unavailable
-
+		Optional<Driver> optionalDriver = driverRepository3.findById(driverId);
+		if(!optionalDriver.isEmpty()) {
+			Driver driver = optionalDriver.get();
+			Cab cab = driver.getCab();
+			cab.setAvailable(Boolean.FALSE);
+		}
 	}
 }
